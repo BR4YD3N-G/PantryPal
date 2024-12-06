aa5a9f77906fa2d2a0444bbd2f131c7450cbcc85
Khanson115 <123264719+Khanson115@users.noreply.github.com>
2024-12-06 15:13:21 -0700

    Update ShoppingList.java

--- pantryPal/src/main/java/pantrypal/ShoppingList.java
+++ pantryPal/src/main/java/pantrypal/ShoppingList.java
@@ -8,12 +8,14 @@ import java.util.List;
  * 
  * @author Kenyon Hanson
  */
+
 public class ShoppingList {
-    private List<pantrypal.ShoppingListItem> items; // List of shopping list items
+    private List<ShoppingListItem> items; // List of shopping list items
 
     /**
      * Constructor to initialize the shopping list.
      */
+    
     public ShoppingList() {
         this.items = new ArrayList<>();
     }
@@ -23,7 +25,8 @@ public class ShoppingList {
      *
      * @param item The item to be added.
      */
-    public void addItem(pantrypal.ShoppingListItem item) {
+    
+    public void addItem(ShoppingListItem item) {
         items.add(item);
     }
 
@@ -33,6 +36,7 @@ public class ShoppingList {
      * @param itemName The name of the item to be removed.
      * @return true if the item was removed successfully, false otherwise.
      */
+    
     public boolean removeItem(String itemName) {
         return items.removeIf(item -> item.getItemName().equalsIgnoreCase(itemName));
     }
@@ -42,13 +46,15 @@ public class ShoppingList {
      *
      * @return A list of shopping list items.
      */
-    public List<pantrypal.ShoppingListItem> listItems() {
+    
+    public List<ShoppingListItem> listItems() {
         return new ArrayList<>(items); // Return a copy to avoid modification.
     }
 
     /**
      * Clears all items from the shopping list.
      */
+    
     public void clearList() {
         items.clear();
     }
